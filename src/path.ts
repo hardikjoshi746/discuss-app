3f5607efa5ecef75fa68898aff587cd5e23e1798
hardikjoshi746 <114323100+hardikjoshi746@users.noreply.github.com>
2025-02-22 14:56:48 -0500

    added the oAuth function

---
+++ src/path.ts
@@ -0,0 +1,17 @@
+const paths = {
+  // the object that contains all the paths of the app
+  home() {
+    return "/";
+  },
+  topicShow(topicSlug: string) {
+    return `/topics/${topicSlug}`;
+  },
+  postCreate(topicSlug: string) {
+    return `/topics/${topicSlug}/posts/new`;
+  },
+  postShow(topicSlug: string, postId: string) {
+    return `/topics/${topicSlug}/posts/${postId}`;
+  },
+};
+
+export default paths;
