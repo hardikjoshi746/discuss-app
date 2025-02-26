d52d4e9286ffe9f9c5bb2f8183f74730c106358c
hardikjoshi746 <114323100+hardikjoshi746@users.noreply.github.com>
2025-02-26 15:45:28 -0500

    added the validation for topic

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,3 +1,83 @@
 "use server";
+import { z } from "zod";
+import { auth } from "@/auth";
+import type { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { revalidatePath } from "next/cache";
+import paths from "@/path";
 
-export async function createTopic() {}
+const createTopicSchema = z.object({
+  // zod schema for topic creation
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Only lowercase letters and dashes are allowed",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  // function to create topic
+  const result = createTopicSchema.safeParse({
+    // validate form data
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const sessions = await auth();
+  if (!sessions || !sessions.user) {
+    return {
+      errors: {
+        _form: ["You must be logged in to create a topic"],
+      },
+    };
+  }
+
+  let topic: Topic;
+
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
+    }
+  }
+
+  redirect(paths.topicShow(topic.slug));
+
+  revalidatePath("/");
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
+import TopicCreateForm from "@/components/topics/topic-create-form";
 
-export default async function Home() {
-  const session = await auth();
-
-  return <div>Home Page</div>;
+export default function Home() {
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
 }
